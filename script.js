ce124dd811dd2b5a58517d83af3bc9db692961d0
Cameron Chandler <webstormcam@gmail.com>
2023-01-09 07:09:42 -0500

    Made the transfer

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const reset = document.getElementById('reset')
 const pagerScreen = document.getElementById('pager-screen')
 const phoneScreen = document.getElementById('phone-screen')
-
+const pageUser = document.getElementById('enter')
 
 reset.addEventListener('click',function(){
 pagerScreen.innerHTML = " "
 phoneScreen.innerHTML = " "
+})
+
+pageUser.addEventListener('click',function(){
+    pagerScreen.innerHTML=phoneScreen.innerHTML
 })
 
 document.addEventListener('click',(e)=>{
