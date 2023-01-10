8c98cb3d4caa25f98808b6921061b3d1786f96e4
Cameron Chandler <webstormcam@gmail.com>
2023-01-09 19:26:19 -0500

    Got the sound to play

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const reset = document.getElementById('reset')
 const pagerScreen = document.getElementById('pager-screen')
 const phoneScreen = document.getElementById('phone-screen')
 const pageUser = document.getElementById('enter')
+let pageSound = new Audio('sound/pager.wav')
 
 reset.addEventListener('click',function(){
 pagerScreen.innerHTML = " "
@@ -9,7 +10,9 @@ phoneScreen.innerHTML = " "
 })
 
 pageUser.addEventListener('click',function(){
+
     pagerScreen.innerHTML=phoneScreen.innerHTML
+    pageSound.play()
 })
 
 document.addEventListener('click',(e)=>{
